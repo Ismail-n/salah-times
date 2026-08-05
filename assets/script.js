22afe4689703cb5efb6f1b63cd6ea3bb4bd0a9e1
Muhammed Ismaeel <ismail@applligent.com>
2026-08-05 07:18:25 +0530

    add even days data and make cards to cover full height

--- assets/script.js
+++ assets/script.js
@@ -23,19 +23,13 @@ fetch("assets/salahData.json")
   .then((response) => response.json())
   .then((data) => {
     const monthData = data[currentMonth];
+    const todayData = monthData ? monthData[currentDate] : null;
 
-
-const todayData = monthData
-  ? Number(currentDate) === 1
-    ? monthData[currentDate]
-    : currentDate %2 === 0 ? monthData[Number(currentDate) - 1] : monthData[currentDate]
-  : null;   
-  
-  console.log("currentDate", currentDate);
     const ul = document.createElement("ul");
     ul.classList.add("salahtime-warpper-ul");
 
     for (const [prayer, time] of Object.entries(todayData)) {
+      console.log(prayer ,time)
       const li = document.createElement("li");
       li.textContent = `${prayer} : ${time}`;
       ul.appendChild(li);
